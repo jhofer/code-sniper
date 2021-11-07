da8286c0b091dbbe304ad20ce20d3c5ff5e8b868
jhofer <jonas.hofer@outlook.com>
2021-11-07 18:57:30 +0100

    inital setup

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Search } from "./search";
+function render() {
+  ReactDOM.render(<Search />, document.body);
+}
+render();
---
+++ src/search.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Search = () => {
+  return <input style={{ width: "100%" }} />;
+};
