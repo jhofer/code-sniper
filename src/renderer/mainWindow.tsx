b064395178d98e11bfcd59426aa3b17ccb3433ff
jhofer <jonas.hofer@outlook.com>
2021-11-10 18:31:42 +0100

    extract component

--- src/renderer/mainWindow.tsx
+++ src/renderer/mainWindow.tsx
@@ -1,35 +1,26 @@
 import { clipboard, ipcRenderer } from "electron";
 import React, { useEffect, useRef, useState } from "react";
-import AceEditor from "react-ace";
 import { SearchBox } from "@fluentui/react/lib/SearchBox";
 import { Stack, IStackTokens } from "@fluentui/react/lib/Stack";
-import { ComboBox, TextField } from "@fluentui/react";
 import { FocusZone } from "@fluentui/react-focus";
 import { CodeBlock, dracula } from "react-code-blocks";
-import "ace-builds/src-noconflict/theme-dracula";
 
-import { languages } from "./languages";
 import { classNames } from "./theme";
 import { useSnippets } from "./useSnippets";
-import { useSnip } from "./useSnip";
 import { PASTE_SNIPPED, SEARCH_SNIP, NEW_SNIP } from "../constants";
+import { NewSnippet } from "./NewSnippet";
 
 export const MainWindow = () => {
-  const [snip, setSnip] = useSnip();
   const [selected, setSelected] = useState(-1);
-  const [description, setDescription] = useState("");
   const [searchText, setSearchText] = useState("");
-  const [language, setLanguage] = useState("typescript");
   const [doCreateSnip, setDoCreateSnip] = useState(false);
   const [snippets, addSnippets] = useSnippets();
+
   const searchBoxRef = useRef(null);
-  const titleFieldRef = useRef(null);
 
   useEffect(() => {
     ipcRenderer.on(NEW_SNIP, () => {
-      titleFieldRef.current?.focus();
       setDoCreateSnip(true);
-      setDescription("");
     });
     ipcRenderer.on(SEARCH_SNIP, () => {
       searchBoxRef.current?.focus();
@@ -45,62 +36,12 @@ export const MainWindow = () => {
   );
 
   return doCreateSnip ? (
-    <Stack style={{ height: "100vh", width: "100vw" }}>
-      <Stack horizontal style={{ width: "100%" }}>
-        <Stack.Item>
-          <ComboBox
-            componentRef={titleFieldRef}
-            text={language}
-            onChange={(e, o, i) => {
-              console.log(o.text);
-              setLanguage(o.text);
-            }}
-            style={{ width: 200 }}
-            allowFreeform={true}
-            autoComplete={"on"}
-            options={languages.map((l) => ({ key: l, text: l }))}
-          />
-        </Stack.Item>
-        <Stack.Item align="stretch" style={{ width: "100%" }}>
-          <TextField
-            style={{ width: "100%" }}
-            placeholder="Snippet Description"
-            value={description}
-            onChange={(ev) => setDescription(ev.currentTarget.value)}
-            onKeyDown={(e) => {
-              if (e.key == "Enter") {
-                const newSnip = { description, snip, language };
-                addSnippets(newSnip);
-                ipcRenderer.send("close-window");
-              }
-            }}
-          />
-        </Stack.Item>
-      </Stack>
-      <AceEditor
-        style={{
-          height: "100%",
-          width: "100%",
-        }}
-        theme="dracula"
-        placeholder="Code here"
-        mode={language}
-        name="basic-code-editor"
-        onChange={(currentCode) => setSnip(currentCode)}
-        fontSize={13}
-        showPrintMargin={true}
-        showGutter={true}
-        highlightActiveLine={true}
-        value={snip}
-        setOptions={{
-          enableBasicAutocompletion: true,
-          enableLiveAutocompletion: true,
-          enableSnippets: true,
-          showLineNumbers: true,
-          tabSize: 4,
-        }}
-      />
-    </Stack>
+    <NewSnippet
+      onSave={(snip) => {
+        addSnippets(snip);
+        ipcRenderer.send("close-window");
+      }}
+    />
   ) : (
     <Stack>
       <SearchBox
@@ -117,7 +58,6 @@ export const MainWindow = () => {
           if (e.key == "Enter") {
             const selectedSnippets = filteredSnippets[selected];
             ipcRenderer.send(PASTE_SNIPPED, selectedSnippets.snip);
-            console.log("select snippet");
           }
         }}
       >
